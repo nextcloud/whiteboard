6847428020f95baa99177307f5dce6a09d2d6632
Julius Härtl <jus@bitgrid.net>
2024-07-05 17:29:51 +0200

    feat: Add metrics endpoint to publish socket.io metrics to prometheus
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- websocket_server/app.js
+++ websocket_server/app.js
@@ -4,11 +4,24 @@
  */
 
 import express from 'express'
+import { register } from 'prom-client'
+
+const METRICS_TOKEN = process.env.METRICS_TOKEN
 
 const app = express()
 
 app.get('/', (req, res) => {
 	res.send('Excalidraw collaboration server is up :)')
+})
+
+app.get('/metrics', async (req, res) => {
+	const token = req.headers.authorization?.split(' ')[1] || req.query.token
+	if (!METRICS_TOKEN || token !== METRICS_TOKEN) {
+		return res.status(403).send('Unauthorized')
+	}
+	const metrics = await register.metrics()
+	res.set('Content-Type', register.contentType)
+	res.end(metrics)
 })
 
 export default app
