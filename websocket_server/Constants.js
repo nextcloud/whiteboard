601c9d4c598ce8efbaac3c4228920964840c057c
Hoang Pham <hoangmaths96@gmail.com>
2025-01-03 16:51:21 +0700

    Fix server crashed, regular cleanups, improve configs, etc...

--- websocket_server/Constants.js
+++ websocket_server/Constants.js
@@ -23,7 +23,7 @@ export const DEFAULT_LOCK_RETRY_INTERVAL = 50
 
 export const DEFAULT_ROOM_CLEANUP_INTERVAL = 5 * 60 * 1000
 
-export const DEFAULT_ROOM_MAX_AGE = 30 * 60 * 1000
+export const DEFAULT_ROOM_MAX_AGE = 5 * 60 * 1000 // 5 minutes, which will also save the room data to nextcloud
 
 export const DEFAULT_CACHED_TOKEN_TTL = 10 * 60 * 1000
 
