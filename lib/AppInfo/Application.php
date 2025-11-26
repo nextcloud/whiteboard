ab2f5d994d6f07d52e0c92c63efa92866161ed61
silver <s.szmajduch@posteo.de>
2025-11-26 17:06:54 +0100

    integrate text editor
    Signed-off-by: silver <s.szmajduch@posteo.de>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -15,6 +15,7 @@ use OCA\Viewer\Event\LoadViewer;
 use OCA\Whiteboard\Listener\AddContentSecurityPolicyListener;
 use OCA\Whiteboard\Listener\BeforeTemplateRenderedListener;
 use OCA\Whiteboard\Listener\LoadViewerListener;
+use OCA\Whiteboard\Listener\LoadTextEditorListener;
 use OCA\Whiteboard\Listener\RegisterTemplateCreatorListener;
 use OCA\Whiteboard\Settings\SetupCheck;
 use OCP\AppFramework\App;
@@ -44,6 +45,7 @@ class Application extends App implements IBootstrap {
 
 		$context->registerEventListener(AddContentSecurityPolicyEvent::class, AddContentSecurityPolicyListener::class);
 		$context->registerEventListener(LoadViewer::class, LoadViewerListener::class);
+		$context->registerEventListener(LoadViewer::class, LoadTextEditorListener::class);
 		$context->registerEventListener(RegisterTemplateCreatorEvent::class, RegisterTemplateCreatorListener::class);
 		$context->registerEventListener(BeforeTemplateRenderedEvent::class, BeforeTemplateRenderedListener::class);
 		$context->registerSetupCheck(SetupCheck::class);
