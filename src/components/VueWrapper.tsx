db121b43e9d5f328e9ea84799b9dc0d57973a51a
Luka Trovic <luka@nextcloud.com>
2026-01-09 16:21:27 +0100

    feat: add sharing hint for embed file, board
    Signed-off-by: Luka Trovic <luka@nextcloud.com>

--- src/components/VueWrapper.tsx
+++ src/components/VueWrapper.tsx
@@ -3,6 +3,9 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 import Vue from 'vue'
+import { t } from '@nextcloud/l10n'
+import { Icon } from '@mdi/react'
+import { mdiAccountPlusOutline } from '@mdi/js'
 
 const VueWrapper = function(
 	{ componentProps, component }) {
@@ -44,7 +47,38 @@ const VueWrapper = function(
 		}
 	}, [Object.values(componentProps)])
 
-	return <div id="vue-component" ref={vueRef}></div>
+	const url = componentProps.text
+
+	// Generate a link to open the details tab for sharing settings
+	let linkToOpenSharingDetails = ''
+	let visibleWarning = ''
+	if (url.includes('/f/')) {
+		linkToOpenSharingDetails = url + '?opendetails=true&openfile=false'
+		visibleWarning = t('whiteboard', 'Please share the file with users; otherwise, they will not be able to see it.')
+	} else if (url.includes('/deck/board/')) {
+		linkToOpenSharingDetails = url + '/details'
+		visibleWarning = t('whiteboard', 'Please share the board with users; otherwise, they will not be able to see it.')
+	}
+
+	if (!linkToOpenSharingDetails) {
+		return <div id="vue-component" ref={vueRef}></div>
+	}
+
+	return (
+		<div>
+			<div style={{
+				padding: '0.5rem',
+				fontStyle: 'italic',
+				color: '#666',
+			}}>
+				{visibleWarning}
+				<a href={linkToOpenSharingDetails} target={'_blank'} style={{ marginLeft: '0.5rem' }}>
+					<Icon path={mdiAccountPlusOutline} size={1} style={{ marginBottom: '-4px' }} />
+				</a>
+			</div>
+			<div id="vue-component" ref={vueRef}></div>
+		</div>
+	)
 }
 
 export default VueWrapper
